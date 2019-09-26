d8938acaf1f29d1177d8d560df9a525ed3ac8b8a
mescolotte <yagoferracini@gmail.com>
2019-09-25 23:02:35 -0400

    controle_acesso

--- Proxy.py
+++ Proxy.py
@@ -10,18 +10,62 @@
 from _thread import *
 from socket import *
 import json
+from datetime import datetime
 
+# ###################### INICIALIZAÇÂO DO PROGRAMA ######################
 print("Iniciando Proxy Python")
 print("Carregando Configurações")
 
 with open("config.json") as data_file:
 	config = json.load(data_file)
 
-proxyIp = config['server ip']
-proxyGate = config['porta']
-
+proxyIp = config['Server ip']
+proxyGate = config['Porta']
+domainNotAllow = config['Domain not Allowed']
+timeNotAllow = config['Time not Allowed']
+daysNotAllow = config['Days not Allowed']
+# wordsNotAllow = config['Words not Allowed']
 
 print("Configurações Carregadas")
+# ###################### FINALIZAÇÂO DA INICIALIZAÇÂO ######################
+
+cache = []  #essa eh nossa cache
+
+
+def controle_acesso(domain):
+	relogioAtual = datetime.now()
+	week = relogioAtual.strftime('%a').upper()
+	time = relogioAtual.strftime('%H:%M')
+	
+	# para o horário n permitido
+	if timeNotAllow[0] < time and time > timeNotAllow[1]:
+		print(time)
+		print("Time Not ALLOWED")
+		return -1
+	
+	# para os dias não permitidos
+	for daysNot in daysNotAllow:
+		if week == daysNot.upper():
+			print("Day Not ALLOWED")
+			return -1
+	
+	# para o domínio não permitido
+	for domainNot in domainNotAllow:
+		x = domain.find(domainNot)
+		if x >= 0:
+			print("Domain Not ALLOWED")
+			return -1
+	
+	return 0
+
+
+def store_cache(request, hora):
+	print(hora)
+	cache.append((request, hora))
+	print(cache)
+	#hora_timeout = datetime.now()
+	#minutos = hora_timeout.minute - hora.minute
+	#print(minutos)
 
 
 def proxy_thread(connection_socket, ):
@@ -57,53 +101,51 @@ def proxy_thread(connection_socket, ):
 		print("DICIONARIO: ", end='')
 		print(thisdict)
 		
-		request = "GET"  # começa montar o pacote de requisicao
-
-		# Checar se a página esta em cache
-		#   Chegar se esta dentro da politica de atualização
-		#   Enviar solicitação ao servidor caso precise
-		#     Obter resposta do servidor
-		# Devolver resposta	ao usuário
-		
 		aux = msg_primary[1][7:]  # Remove 'http://'
 		aux = aux.split("/", 1)  # Separates domain from the data
 		domain = aux[0]
 		caminho = " /"  # monta o caminho necessário para requisição
+		
 		if len(aux) > 1:  # garante a execução, caso n haja aux[1]
 			caminho += aux[1]
 		
 		print("DOMAIN: " + domain)
+		allowed = controle_acesso(domain) #controle de acesso do dominio
+		if allowed == 0:
+			try:
+				# catch the ip from the domain
+				ip = gethostbyname(domain)
+				print("IP: " + ip)
+			except:
+				connection_socket.send(bytes("HTTP/1.1 502 Bad Gateway\r\n", "utf-8"))
+			
+			# Monta a requisição
+			request = "GET" + caminho + " " + msg_primary[2] + "\r\n"
 
-		try:
-			# catch the ip from the domain
-			ip = gethostbyname(domain)
-			print("IP: " + ip)
-		except:
-			connection_socket.send(bytes("HTTP/1.1 502 Bad Gateway\r\n", "utf-8"))
-		
-		request += caminho + " " + msg_primary[2] + "\r\n"
-		if len(msg2) > 1:
-			request += msg2[1]
-		print("REQUEST: ", end = '')
-		print(bytes(request, "utf-8"))
-		
-		try:
-			# open the TCP connection
-			server_socket.connect((ip, 80))
-			
-			server_socket.send(bytes(request, "utf-8"))
-			
-			# Receive the data from server
-			dados = server_socket.recv(4096)
-			# http = server_socket.recv(4096).decode("utf-8")
-			
-			print("DADOS: ", end='')
-			print(dados)
-			connection_socket.send(dados)
-			# connection_socket.send(bytes(http, "utf-8"))
-		except:
-			connection_socket.send(bytes("HTTP/1.1 502 Bad Gateway\r\n", "utf-8"))
-		
+			hora = datetime.now()
+			store_cache(request, hora)  # TODO penso que request esta equivocado, prefira (domain + " /" + caminho) e não esqueça de salvar a pagina q recebemos
+			if len(msg2) > 1:
+				request += msg2[1]
+			print("REQUEST: ", end='')
+			print(bytes(request, "utf-8"))
+
+			try:
+				# open the TCP connection
+				server_socket.connect((ip, 80))
+
+				server_socket.send(bytes(request, "utf-8"))
+
+				# Receive the data from server
+				dados = server_socket.recv(4096)
+
+				print("DADOS: ", end='')
+				print(dados)
+				connection_socket.send(dados)
+			except:
+				connection_socket.send(bytes("HTTP/1.1 502 Bad Gateway\r\n", "utf-8"))
+		else:
+			connection_socket.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))  # TODO Com a pagina de not allowed
+
 		# close the connection and server sockets
 		server_socket.close()
 		connection_socket.close()
